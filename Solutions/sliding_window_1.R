f6258f5bbea34ac956a3c438d71947b34351f039
Ed Kohlwey <ekohlwey@gmail.com>
2012-10-17 15:01:45 -0400

    Added slightly better plot.

--- Solutions/sliding_window_1.R
+++ Solutions/sliding_window_1.R
@@ -4,6 +4,11 @@ library(topicmodels)
 
 rmr.options.set(backend="local")
 
+stop_file = file("~/Data/stopwords.txt")
+stopwords = readLines(stop_file)
+close(stop_file)
+stopexpr = do.call(paste, args=as.list(c(stopwords, sep=" | ")))
+
 time_extractor = function(tweet) {
   floor(unclass(as.POSIXct(
     tweet["created_at"], format='%a, %d %b %Y %X %z', tz="UTC"
@@ -13,7 +18,13 @@ time_extractor = function(tweet) {
 tweet_mapper = function(null,tweet_text) {
   tweet = fromJSON(tweet_text)
   tweet_time_frame = time_extractor(tweet)
-  tweet_text = tweet["text"]
+  tweet_text = gsub(pattern=",|'",
+                    x=tweet["text"], perl=T, replacement="")
+  tweet_text = gsub(pattern="-|\\?|\\.|;", x=tweet_text, perl=T, replacement="")
+  tweet_text = gsub(pattern=stopexpr, x=tweet_text, perl=T, replacement="")
+  tweet_text = gsub(pattern="^\\s+|\\s+$", x = tweet_text, perl=T, replacement="")
+  tweet_text = gsub(pattern="\\s+\\w\\s", x = tweet_text, perl=T, replacement="")
+  tweet_text = gsub(pattern="\\s+", x = tweet_text, perl=T, replacement=" ")
   if(nchar(tweet_text)>20)
     keyval(tweet_time_frame, tweet_text)
   else
@@ -23,19 +34,28 @@ tweet_mapper = function(null,tweet_text) {
 error_func= function(e) print(e)
 
 topics_reducer = function(time,tweets){
+  tryCatch({
     window_corpus=Corpus(VectorSource(tweets))
     window_matrix=DocumentTermMatrix(window_corpus, )
                # control = list(removePunctuation=T, stopwords=T))
     topic_model=LDA(window_matrix,3) #adjust the number of topics here
     ret = keyval(time, topic_model)
     return(ret)
+  }, error = function(e) print(tweets))
 }
 
-tweet_timeframes = from.dfs(mapreduce("~/Data/sample_twitter_data",
+tweet_timeframes = from.dfs(mapreduce("~/Data/small_sample_twitter_data",
                                       input.format="text",
                                       map=tweet_mapper,
                                       reduce=topics_reducer
                                      ))
 
-terms = lapply(tweet_timeframes, function(kv) terms(kv$val))
-frequencies = lapply(tweet_timeframes, function(kv) posterior(kv$val))
+n_results = 5
+terms = lapply(tweet_timeframes, function(kv) terms(kv$val, n_results))
+topics = lapply(tweet_timeframes, function(kv){
+  topics = posterior(kv$val)$topics
+  topics = topics[order(topics, decreasing=T)]
+  topics[1:n_results]
+})
+
+barplot(unlist(topics)[1:30]-.99, names.arg=unlist(terms)[1:30], col=rainbow(5))
