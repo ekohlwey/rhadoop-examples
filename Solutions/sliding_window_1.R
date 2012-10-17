2bc8e3bc73a308733f0c1956e71c716b4412bea7
Ed Kohlwey <ekohlwey@gmail.com>
2012-10-17 09:49:01 -0400

    Fixed error with small number of terms in tweets for LDA example.

--- Solutions/sliding_window_1.R
+++ Solutions/sliding_window_1.R
@@ -6,23 +6,30 @@ rmr.options.set(backend="local")
 
 time_extractor = function(tweet) {
   floor(unclass(as.POSIXct(
-    tweet$created_at, format='%a, %d %b %Y %X %z', tz="UTC"
+    tweet["created_at"], format='%a, %d %b %Y %X %z', tz="UTC"
   ))[1]/3600)
 }
 
 tweet_mapper = function(null,tweet_text) {
   tweet = fromJSON(tweet_text)
   tweet_time_frame = time_extractor(tweet)
-  keyval(tweet_time_frame, tweet$text)
+  tweet_text = tweet["text"]
+  if(nchar(tweet_text)>20)
+    keyval(tweet_time_frame, tweet_text)
+  else
+    return()
 }
 
+error_func= function(e) print(e)
+
 topics_reducer = function(time,tweets){
-    tryCatch({
-      window_corpus=Corpus(VectorSource(tweets))
-      window_matrix=DocumentTermMatrix(window_corpus)
-      topic_model=LDA(window_matrix,3) #adjust the number of topics here
-      keyval(time, topic_model)
-    }, error= function(e){return()})
+    window_corpus=Corpus(VectorSource(tweets))
+    window_matrix=DocumentTermMatrix(window_corpus, )
+               # control = list(removePunctuation=T, stopwords=T))
+    topic_model=LDA(window_matrix,3) #adjust the number of topics here
+    ret = keyval(time, topic_model)
+    print (ret)
+    return(ret)
 }
 
 tweet_timeframes = from.dfs(mapreduce("~/Data/sample_twitter_data",
@@ -31,7 +38,3 @@ tweet_timeframes = from.dfs(mapreduce("~/Data/sample_twitter_data",
                                       reduce=topics_reducer
                                      ))
 
-first_topic_weights = lapply(tweet_timeframes, function(kv) attr(unclass(kv$val),"beta")[1,] )
-frame_terms = lapply(tweet_timeframes, function(kv) attr(unclass(kv$val), "terms"))
-top_term_orders = sapply(first_topic_weights, function(weights) order(sapply(weights, function(x)10^x), decreasing=T)[1:4])
-barplot(unlist(first_topic_weights)[top_term_orders[1:40]], names.arg=unlist(frame_terms)[top_term_orders[1:40]])
